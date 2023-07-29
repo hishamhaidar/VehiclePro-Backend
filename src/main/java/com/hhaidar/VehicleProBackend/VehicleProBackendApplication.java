8f6d1dfa84aea7c794d12fa4e2d5a5cc79ab4783
hishamhaidar <hisham.haidar2018@hotmail.com>
2023-07-29 10:06:54 +0200

    initializing

---
+++ src/main/java/com/hhaidar/VehicleProBackend/VehicleProBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hhaidar.VehicleProBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleProBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleProBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hhaidar/VehicleProBackend/VehicleProBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhaidar.VehicleProBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleProBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
