545564d7d670a4afd498a81d5ad5567f0e631020
hishamhaidar <hisham.haidar2018@hotmail.com>
2023-07-30 10:29:36 +0200

    switching to java8,spring security 3-

--- src/main/java/com/hhaidar/VehicleProBackend/model/User.java
+++ src/main/java/com/hhaidar/VehicleProBackend/model/User.java
@@ -1,42 +1,44 @@
 package com.hhaidar.VehicleProBackend.model;
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+
+import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import javax.persistence.*;
 import java.util.Collection;
+import java.util.Collections;
 
 @Entity
 @Data
-@AllArgsConstructor
+@EqualsAndHashCode
 @NoArgsConstructor
+@Table(name = "user_details")
 public class User implements UserDetails {
     @Id
-    @GeneratedValue
+    @SequenceGenerator(name = "user_id_gen",
+            sequenceName = "user_id_gen",
+            allocationSize = 1)
+    @GeneratedValue(strategy =GenerationType.SEQUENCE
+            ,generator = "user_id_gen")
     private Integer userID;
 
     @Column(name = "username")
     private String username;
-
     @Column(name = "email")
     private String userEmail;
-
     @Column(name = "password")
     private String userPassword;
 
     @Enumerated(EnumType.STRING)
     private Role role;
 
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         SimpleGrantedAuthority authority = new SimpleGrantedAuthority(this.role.name());
-        return Arrays.asList(authority);
+        return Collections.singletonList(authority);
     }
 
     @Override
@@ -52,6 +54,14 @@ public class User implements UserDetails {
     @Override
     public boolean isAccountNonExpired() {
         return true;
+    }
+
+    public User(String username, String userEmail, String userPassword) {
+        this.username = username;
+        this.userEmail = userEmail;
+        this.userPassword = userPassword;
+        this.role = Role.NEW_USER;
+
     }
 
     @Override
--- src/main/java/com/hhaidar/VehicleProBackend/repository/UserRepo.java
+++ src/main/java/com/hhaidar/VehicleProBackend/repository/UserRepo.java
@@ -3,11 +3,13 @@ package com.hhaidar.VehicleProBackend.repository;
 import com.hhaidar.VehicleProBackend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Repository
+@Transactional(readOnly = true)
 public interface UserRepo extends JpaRepository<User,Integer> {
 
-    Optional<User> findByUsername(String username);
+    Optional<User> findUserByEmail(String email);
 }
